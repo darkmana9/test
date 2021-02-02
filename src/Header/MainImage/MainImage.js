36ee0896685353daa39b607df9c8521788a5deb1
darkmana9 <aka.cc@mail.ru>
2021-02-02 15:48:56 +0300

    added travis cli

--- src/Header/MainImage/MainImage.js
+++ src/Header/MainImage/MainImage.js
@@ -4,7 +4,7 @@ import c from "./MainImage.module.css"
 const MainImage = () => {
     return (
         <div className={c.mainImage__wrapper}>
-<img className={c.mainImage} src='https://html.crumina.net/html-olympus/img/top-header1.jpg'/>
+<img alt="123" className={c.mainImage} src='https://html.crumina.net/html-olympus/img/top-header1.jpg'/>
         </div>
     );
 }
