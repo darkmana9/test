93a7e2b7f87325f9ef9f5cc71bce97dec73dd6d8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-30 05:55:50 +0000

    Deploy darkmana9/test to github.com/darkmana9/test.git:gh-pages

---
+++ precache-manifest.986e57b4d8e251a237a02dac4bc04a33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fba1884665f9fb50d37003362b6e49",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "d641d0cb8860d4b61d6a",
+    "url": "/test/static/css/main.9d5a7480.chunk.css"
+  },
+  {
+    "revision": "fa923b7a7cdf11a62c2f",
+    "url": "/test/static/js/2.4097cb8f.chunk.js"
+  },
+  {
+    "revision": "59c4dee7f1aeea24999a0e350db2af8b",
+    "url": "/test/static/js/2.4097cb8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d641d0cb8860d4b61d6a",
+    "url": "/test/static/js/main.0040f348.chunk.js"
+  },
+  {
+    "revision": "4d1949d49ef5c74ac638",
+    "url": "/test/static/js/runtime-main.1c1c7439.js"
+  }
+]);
